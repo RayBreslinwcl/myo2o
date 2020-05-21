c8d3acb1f99d9f21205bde0d19bc69973968c9ce
Raywcl <2728928100@qq.com>
2020-05-21 11:12:09 +0800

    初始化项目 1.jdbc的用户名和密码需要加密后写入
    遗留问题
    redis相关bean依旧报红线在spring-redis.xml配置里面

---
+++ src/main/java/com/imooc/myo2o/util/DESUtils.java
@@ -0,0 +1,74 @@
+package com.imooc.myo2o.util;
+
+import java.security.Key;
+import java.security.SecureRandom;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
+public class DESUtils {
+
+	private static Key key;
+	private static String KEY_STR = "myKey";
+	private static String CHARSETNAME = "UTF-8";
+	private static String ALGORITHM = "DES";
+
+	static {
+		try {
+			KeyGenerator generator = KeyGenerator.getInstance(ALGORITHM);
+			SecureRandom secureRandom = SecureRandom.getInstance("SHA1PRNG");
+			secureRandom.setSeed(KEY_STR.getBytes());
+			generator.init(secureRandom);
+			key = generator.generateKey();
+			generator = null;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public static String getEncryptString(String str) {
+		BASE64Encoder base64encoder = new BASE64Encoder();
+		try {
+			byte[] bytes = str.getBytes(CHARSETNAME);
+			Cipher cipher = Cipher.getInstance(ALGORITHM);
+			cipher.init(Cipher.ENCRYPT_MODE, key);
+			byte[] doFinal = cipher.doFinal(bytes);
+			return base64encoder.encode(doFinal);
+		} catch (Exception e) {
+			// TODO: handle exception
+			throw new RuntimeException(e);
+		}
+	}
+
+
+	public static String getDecryptString(String str) {
+		//基于BASE64编码，接收byte[]并且转换成string
+		BASE64Decoder base64decoder = new BASE64Decoder();
+		try {
+			//将字符串decode成byte[]
+			byte[] bytes = base64decoder.decodeBuffer(str);
+			//获取解密对象
+			Cipher cipher = Cipher.getInstance(ALGORITHM);
+			//初始化解密信息
+			cipher.init(Cipher.DECRYPT_MODE, key);
+			//解密
+			byte[] doFinal = cipher.doFinal(bytes);
+			//返回解密后的信息
+			return new String(doFinal, CHARSETNAME);
+		} catch (Exception e) {
+			// TODO: handle exception
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(getEncryptString("root"));
+		System.out.println(getEncryptString("123456"));
+//		System.out.println(getEncryptString("wxd7f6c5b8899fba83"));
+//		System.out.println(getEncryptString("665ae80dba31fc91ab6191e7da4d676d"));
+	}
+
+}
